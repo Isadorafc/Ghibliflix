8ddce51cccfed17908bfb4db29cd02cb5ba23408
Isadora Ferreira <isaferrercav@gmail.com>
2020-07-29 17:32:24 -0300

    aula 2 finalizada

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   return (
     <PageDefault>
       <h1>Cadastrar Categoria</h1>
-      <Link to="/cadastro/categoria">Votar</Link>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+        <button>Cadastrar</button>
+      </form>
+      <Link to="/">Ir para página inicial</Link>
     </PageDefault>
   );
 }
